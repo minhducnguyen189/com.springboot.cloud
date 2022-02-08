06f841d711bda96025f2480378e47e7a2cbbd063
minhducnguyen189 <minhducnguyen189@gmail.com>
2022-02-08 20:08:24 +0700

    update OAP server

--- openfeign-openapi-server/src/main/java/com/springboot/cloud/openfeign/openapi/server/app/controller/ServerController.java
+++ openfeign-openapi-server/src/main/java/com/springboot/cloud/openfeign/openapi/server/app/controller/ServerController.java
@@ -1,18 +1,31 @@
 package com.springboot.cloud.openfeign.openapi.server.app.controller;
 
-import com.springboot.cloud.openfeign.openapi.server.app.api.DatetimeApi;
+import com.springboot.cloud.openfeign.openapi.server.app.api.V1Api;
+import com.springboot.cloud.openfeign.openapi.server.app.model.Customer;
+import com.springboot.cloud.openfeign.openapi.server.app.model.CustomerRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-public class ServerController implements DatetimeApi {
+public class ServerController implements V1Api {
 
     @Override
-    public ResponseEntity<String> getServerDateTime() {
-        return new ResponseEntity<>(LocalDateTime.now().toString(), HttpStatus.OK);
+    public ResponseEntity<UUID> createCustomer(CustomerRequest customerRequest) {
+        return new ResponseEntity<>(UUID.randomUUID(), HttpStatus.CREATED);
     }
+
+    @Override
+    public ResponseEntity<List<Customer>> getCustomers() {
+        Customer customer = new Customer();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(customer);
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
 
 }
