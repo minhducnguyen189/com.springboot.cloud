cb0bb442d3736e6988dc12c54d9d3a1d7ba4517e
nguyenminhduc189 <duc.nguyen@ubitec.com>
2022-03-16 16:04:33 +0700

    update name of AdapterServerApi FeignClient

--- bff-application-service/src/main/java/com/application/bff/adapters/AdapterServerApi.java
+++ bff-application-service/src/main/java/com/application/bff/adapters/AdapterServerApi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "BLOG-ADAPTER-SERVICE")
+@FeignClient(name = "blog-adapter-service")
 public interface AdapterServerApi {
 
     @RequestMapping(method = RequestMethod.GET, value = "/v1/sayHello")
