b7c2e9a90d09c7fbf73c821ec0d5991d3c6edc77
ducnguyen <minhducnguyen189@gmail.com>
2022-12-23 16:19:36 +0700

    update spring security config

--- openfeign-interceptor-service/src/main/java/com/springboot/cloud/openfeign/interceptor/service/config/SecurityConfig.java
+++ openfeign-interceptor-service/src/main/java/com/springboot/cloud/openfeign/interceptor/service/config/SecurityConfig.java
@@ -1,17 +1,19 @@
 package com.springboot.cloud.openfeign.interceptor.service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
     }
 
 }
