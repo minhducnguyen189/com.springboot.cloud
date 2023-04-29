c57e93d72d931c62f2698f628f6825f52e0976a8
ducnguyen <minhducnguyen189@gmail.com>
2023-04-29 10:33:36 +0700

    spring cloud sleuth with zipkin

---
+++ sleuth-zipkin-eureka-server/src/main/java/com/springboot/cloud/sleuth/zipkin/eureka/server/SleuthZipkinEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.cloud.sleuth.zipkin.eureka.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SleuthZipkinEurekaServerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
