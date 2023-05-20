01c969794d7b611fc5906f43eb174d03dac16687
minhducnguyen189 <minhducnguyen189@gmail.com>
2023-05-20 13:29:01 +0700

    config Sleuth to print parent Span Id

---
+++ sleuth-log4j2-kafka-zipkin-product-service/src/main/java/com/springboot/cloud/sleuth/log4j2/kafka/zipkin/product/service/config/SleuthConfig.java
@@ -0,0 +1,22 @@
+package com.springboot.cloud.sleuth.log4j2.kafka.zipkin.product.service.config;
+
+import brave.baggage.BaggageFields;
+import brave.baggage.CorrelationScopeConfig;
+import brave.baggage.CorrelationScopeCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SleuthConfig {
+
+    @Bean
+    CorrelationScopeCustomizer addSampled() {
+        return b -> b.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.SAMPLED));
+    }
+
+    @Bean
+    CorrelationScopeCustomizer addParentId() {
+        return b -> b.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.PARENT_ID));
+    }
+
+}
